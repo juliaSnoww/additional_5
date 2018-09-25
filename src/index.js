1f411d755b5a02b8261c3c1ccffc8fb3df261fd0
Julia Pritychenko <juliasnoww@mail.ru>
2018-09-25 20:05:04 +0300

    hard

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let array = [];
+    str = str.split("");
+    brackets = bracketsConfig;
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0, ln = brackets.length; j < ln; j++) {
+            if (str[i] === brackets[j][0] || str[i] === brackets[j][1]) {
+
+                if (brackets[j][0] === brackets[j][1]) {
+                    if (array[array.length - 1] === brackets[j][0]) {
+                        array.pop();
+                        break;
+                    }
+                    array.push(str[i]);
+                    break;
+                }
+
+                if (str[i] === brackets[j][0]) {
+                    array.push(str[i]);
+                    break;
+                }
+
+                if (array[array.length - 1] !== brackets[j][0] || array.length === 0) {
+                    return false;
+                }
+                array.pop();
+            }
+        }
+    }
+    if (array.length === 0) return true;
+    else return false;
+};
